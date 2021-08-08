35e291e47394ccf48ce4028dc0b432cb28f4dfcd
Joshua Hiles <joshhiles1@live.co.uk>
2021-08-08 11:51:03 +0100

    Add initial & codeql

---
+++ net6codeql/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace net6codeql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
